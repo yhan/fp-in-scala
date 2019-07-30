a64ffc7ebbeba2224246f479f106b25a04c7ad5a
Yi HAN <hanyi@8110@gmail.com>
2019-07-30 16:08:03 +0200

    tree depth

--- src/main/scala/collections/Tree.scala
+++ src/main/scala/collections/Tree.scala
@@ -9,7 +9,7 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
     def depth[A](tree: Tree[A]): Int = tree match {
         case Leaf(_) => 1
-        case Branch(left, right) => (1 + depth(left)) max (1 + depth(right))
+        case Branch(l, r) => 1 + (depth(l) max depth(r))
     }
 
     def maximum(t: Tree[Int]): Int = t match {
